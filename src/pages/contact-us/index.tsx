e6f5a111ea3a16118905390357afad4b1d06e88e
Osama Dahnoun <odahnoun@cisco.com>
2025-08-13 08:26:14 -0400

    test change to see if comms with backend working

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -2,6 +2,7 @@ export const ContactUs = () => {
   return (
     <div className="mt-10 flex items-center justify-center">
       <h2 className="text-lg">Contact us</h2>
+      <p>Test gcp change</p>
     </div>
   );
 };
